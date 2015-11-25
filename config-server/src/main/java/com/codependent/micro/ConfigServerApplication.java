0753f98ca7b27db0cbf453888bfb8e20dcb5786c
codependent <joseantonio.inigo@gmail.com>
2015-11-26 00:14:11 +0100

    config repo

--- config-server/src/main/java/com/codependent/micro/ConfigServerApplication.java
+++ config-server/src/main/java/com/codependent/micro/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.codependent.micro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
