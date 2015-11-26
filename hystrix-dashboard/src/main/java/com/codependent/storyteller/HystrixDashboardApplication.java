16b5341466b12191827026a167165813eb284983
codependent <joseantonio.inigo@gmail.com>
2015-11-27 00:49:53 +0100

    turbine update

--- hystrix-dashboard/src/main/java/com/codependent/storyteller/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/codependent/storyteller/HystrixDashboardApplication.java
@@ -3,8 +3,10 @@ package com.codependent.storyteller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @EnableHystrixDashboard
+@EnableTurbine
 @SpringBootApplication
 public class HystrixDashboardApplication {
 
