a07406e2abcdcdc7ad5c939646b6d05fd2b97c55
codependent <joseantonio.inigo@gmail.com>
2015-11-26 10:25:21 +0100

    config server

--- config-server/src/main/java/com/codependent/micro/ConfigServerApplication.java
+++ config-server/src/main/java/com/codependent/micro/ConfigServerApplication.java
@@ -3,8 +3,10 @@ package com.codependent.micro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
