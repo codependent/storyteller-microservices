e219e3b584d95fa8e69865775f1e317c164f0698
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2017-01-23 18:18:55 +0100

    dockerizing

--- archimedes/startContainer.sh
+++ archimedes/startContainer.sh
@@ -1,4 +1,6 @@
 mvn clean package docker:build -DskipTests
-docker run -e "SPRING_PROFILES_ACTIVE=archimedes1" -p 8761:8761 -d storyteller/archimedes 
-docker run -e "SPRING_PROFILES_ACTIVE=archimedes2" -p 8762:8762 -d storyteller/archimedes
-
+docker run -e "SPRING_PROFILES_ACTIVE=archimedes1" -p 8761:8761 \
+  --name archimedes1 --add-host archimedes1:192.168.99.100 --add-host archimedes2:192.168.99.100 --add-host my-rabbit:192.168.99.100 \
+  -d storyteller/archimedes 
+docker run -e "SPRING_PROFILES_ACTIVE=archimedes2" -p 8762:8762 \
+  --name archimedes2 --add-host archimedes1:192.168.99.100 --add-host archimedes2:192.168.99.100 --add-host my-rabbit:192.168.99.100 -d storyteller/archimedes
---
+++ config-server/startContainer.sh
@@ -0,0 +1,7 @@
+mvn clean package docker:build -DskipTests
+docker run -p 5672:5762 \
+  --hostname my-rabbit --name my-rabbit --add-host archimedes1:192.168.99.100 --add-host archimedes2:192.168.99.100 \
+  -d rabbitmq:3
+docker run -p 8888:8888 --name config-server \
+  --add-host archimedes1:192.168.99.100 --add-host archimedes2:192.168.99.100 --add-host my-rabbit:192.168.99.100 \
+  -d storyteller/config-server
