df6c0017545753df8a7e55de32308e153f236c25
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2017-01-24 09:24:52 +0100

    rabbit mq port fixed

--- config-server/startContainer.sh
+++ config-server/startContainer.sh
@@ -1,5 +1,5 @@
 mvn clean package docker:build -DskipTests
-docker run -p 5672:5762 \
+docker run -p 5672:5672 \
   --hostname my-rabbit --name my-rabbit --add-host archimedes1:192.168.99.100 --add-host archimedes2:192.168.99.100 \
   -d rabbitmq:3
 docker run -p 8888:8888 --name config-server \
