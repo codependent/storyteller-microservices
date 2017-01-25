068e8224d30528ccb14968b7c53353f6deae1b4f
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2017-01-25 23:53:41 +0100

    fully dockerized

---
+++ gatekeeper/startContainer.sh
@@ -0,0 +1,2 @@
+mvn clean package docker:build -DskipTests
+docker run -p 9966:9966 --name gatekeeper --add-host archimedes1:192.168.99.100 --add-host archimedes2:192.168.99.100 --add-host config-server:192.168.99.100 --add-host my-rabbit:192.168.99.100 --add-host gatekeeper:192.168.99.100 --add-host stories-microservice:192.168.99.100 --add-host images-microservice:192.168.99.100 --add-host storyteller-api:192.168.99.100 --add-host hystrix-dashboard:192.168.99.100 -d storyteller/gatekeeper
